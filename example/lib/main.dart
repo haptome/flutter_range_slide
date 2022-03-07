755a5b3c77b4e9061e68e94f80b538c514fd1d83
haptome <haptome2@icloud.com>
2022-03-07 12:56:16 +0300

    Update main.dart
    pkg name changed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_range_slider/flutter_range_slider.dart';
+import 'package:range_slider_flutter/range_slider_flutter.dart';
 
 void main() {
   runApp(const MyApp());
